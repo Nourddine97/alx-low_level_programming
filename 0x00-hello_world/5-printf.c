ceba6aa6580ee781e3453a8796c643286a185506
Nourddine97 <hassnmaghouzi@gmail.com>
2023-05-20 06:38:41 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * description: a c program that prints function.
- *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
