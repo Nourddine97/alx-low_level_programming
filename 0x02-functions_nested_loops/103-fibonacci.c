d764341dcd4cd999e6a8415f00bbeb396031eceb
Nourddine97 <hassnmaghouzi@gmail.com>
2023-05-23 15:24:37 -0700

    a

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,48 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - fibonacci <3
+ * main - Entry point
  *
- * Purpose - no hardcode
+ * Description: Prints the sum of even-valued
+ *		Fibonacci sequence not exceed
+ *		4million
  *
- * Return:  (Success)
- */
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
+	while (1)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
+		sum = fib1 + fib2;
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+		if (sum > 4000000)
+			break;
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
 	}
-	printf("\n");
+	printf("%.0f\n", total_sum);
+
 	return (0);
-}
 }
